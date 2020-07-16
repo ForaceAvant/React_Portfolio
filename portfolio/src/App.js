e23b6cb195f693db2fe77356b8feba09d22cb444
Forace Avant <themattavant@gmail.com>
2020-07-15 18:59:45 -0700

    Created React App and set up Navbar

---
+++ portfolio/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from'react-bootstrap/Navbar';
+import Nav from'react-bootstrap/Nav';
+import './App.css';
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      title: "Forace Avant",
+      headerLinks: [
+        {title: 'Home', path: '/'},
+        {title: 'About', path: '/about'},
+        {title: 'Contact', path: '/contact'}
+      ],
+      home: {
+        title: 'SAMPLE TITLE',
+        subTitle: 'SAMPLE SUBTITLE',
+        text: 'SAMPLE SMALLER TEXT'
+      },
+      about: {
+        title: 'SAMPLE TITLE',
+      },
+      contact: {
+        title: 'SAMPLE TITLE',
+      }
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className='border-bottom' bg="transparent" expand="lg">
+            <Navbar.Brand>Forace Avant</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </Router>
+  );
+  }
+}
+
+export default App;
