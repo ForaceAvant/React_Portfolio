5d8f51d8240b5b73c1796fcc83d0f10f6d9eb321
Forace Avant <themattavant@gmail.com>
2020-07-15 21:02:04 -0700

    Put header material in App.js because there were issues routing between pages. Also added home about and contact pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 import './App.css';
 
@@ -36,10 +40,25 @@ class App extends React.Component{
     return (
       <Router>
         <Container className="p-0" fluid={true}>
-          <Header />
+        <Navbar className='border-bottom' bg="transparent" expand="lg">
+                <Navbar.Brand>Forace Avant</Navbar.Brand>
+                <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+                <Navbar.Collapse id="navbar-toggle">
+                    <Nav className="ml-auto">
+
+                        <Link className="nav-link" to="/">Home</Link>
+                        <Link className="nav-link" to="/about">About</Link>
+                        <Link className="nav-link" to="/contact">Contact</Link>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+            <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text= {this.state.home.text} />} />
+            <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+            <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
 
           <Footer />
-
         </Container>
       </Router>
   );
---
+++ portfolio/src/pages/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function AboutPage(props){
+
+    return(
+        <p>AboutPAge Works</p>
+    );
+}
+
+export default AboutPage;
